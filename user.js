ee27ffc7d94db868607a19902ba8da5cc748351b
soulhotel <155501797+soulhotel@users.noreply.github.com>
2024-07-17 23:34:20 -0400

    Update user.js

--- user.js
+++ user.js
@@ -1,39 +1,20 @@
+/*color scheme*/
+
 /*tabs*/
-user_pref("ultima.tabs.size.xs", false);
-user_pref("ultima.tabs.size.s", false);
-user_pref("ultima.tabs.size.l", true);
-user_pref("ultima.tabs.autohide", true);
-user_pref("ultima.tabs.closetabsbutton", true);
-user_pref("ultima.tabs.vertical.hide", false);
-user_pref("ultima.tabs.vertical.onlyprivate", false);
 
 /*sidebar*/
 user_pref("ultima.sidebar.autohide", false);
 
-/*extension theme on-off*/
-user_pref("ultima.theme.extensions", true);
+/*extension theme*/
 
 /*url bar*/
-user_pref("ultima.urlbar.suggestions", true);
-user_pref("ultima.urlbar.centered", true);
-user_pref("ultima.urlbar.hidebuttons", false);
+
+/*context menu*/
+user_pref("ultima.contextmenu.declutter", false);
 
 /*alternate styles*/
-user_pref("ultima.xstyle.containertabs.i", false);
-user_pref("ultima.xstyle.containertabs.ii", false);
-user_pref("ultima.xstyle.containertabs.iii", true);
-user_pref("ultima.xstyle.squared", false);
-user_pref("ultima.xstyle.lwtheme", false); /* Experimental. To counter addon themes that use unusual color variables. */
-user_pref("ultima.xstyle.pinnedtabs.i", false); /* Experimental. Havent decided on style yet. */
-
 
 /*specific OS overrides (like titlebar buttons)*/
-user_pref("ultima.OS.kde", true);
-user_pref("ultima.OS.gnome", false);
-user_pref("ultima.OS.mac", false);
-/*user_pref("ultima.OS.linux", false);*/ /*Experimental*/
 
 /*other*/
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("svg.context-properties.content.enabled", true);
-user_pref("toolkit.tabbox.switchByScrolling", true);
+
